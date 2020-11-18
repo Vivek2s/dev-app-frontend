9e054f525597ff7d6583a8897ba313f2f8898a4d
vivek <vivek.kumar@venturepact.com>
2020-11-18 12:41:35 +0530

    added environment file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    ENV_NAME: 'staging',
+    API: 'https://pacific-lake-30572.herokuapp.com/api/v1',
+    PROTOCOL: 'https://',
+    CLIENT_ID: 'devapp',
+    CLIENT_SECRET: '$2b$10$XMsZVFqWzPbJbHuIDQUckubRPPL0OrIVVSoIRqzBloaHWhmg/pk6W',
+    APP_EXTENSION: 'devapp.local'
+  }
